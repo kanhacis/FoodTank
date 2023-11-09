8ce1c12bb803f3bc93c14a6296216c3011919449
kanhacis <kanha.p@cisinlabs.com>
2023-11-09 16:35:47 +0530

    Commit four

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,5 +4,10 @@ from .import views
 urlpatterns = [
     path("restaurant/", views.restaurant, name="restaurant"),
     path("addRestaurant/", views.addRestaurant, name="addRestaurant"),
+    path("deleteRestaurant/<int:id>", views.deleteRestaurant, name="deleteRestaurant"),
+    path("addMenu/", views.addMenu, name="addMenu"),
+    path("viewMenu/<int:id>", views.viewMenu, name="viewMenu"),
+    path("deleteMenu/<int:id>", views.deleteMenu, name="deleteMenu"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path("restaurant_info/<int:id>", views.restaurant_info, name="restaurant_info"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,14 +3,14 @@ from .models import Restaurant
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from user.models import User
+from menu.models import Menu
 
 
 # Function (foodprovider dashboard)
-# @login_required('/login/') # AttributeError: 'function' object has no attribute 'user'
 def dashboard(request):
     # Check if the user is authenticated, if not, redirect them to the owner login page
     if not request.user.is_authenticated:
-        return redirect("ownerlogin")
+        return redirect("/login/")
     
     # Get the restaurant admin user object based on the currently logged-in user
     restaurant_admin = User.objects.get(username=request.user)
@@ -27,23 +27,25 @@ def dashboard(request):
     return render(request, 'foodprovider/restaurant_dashboard.html', context)
 
 # Function (Add restaurants)
-# @login_required('/login/')
 def addRestaurant(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
     if request.method == 'POST':
         # Get the restaurant data from the add_restaurant.html template
-        uname = request.POST['uname']
-        rname = request.POST['rname']
-        rcity = request.POST['rcity']
-        raddress = request.POST['raddress']
-        rmobile = request.POST['rmobile']
-        veg = request.POST['veg']
-        nchefs = request.POST['nchefs']
-        # rdate = request.POST['rdate']
+        uname = request.POST.get('uname')
+        rname = request.POST.get('rname','')
+        rcity = request.POST.get('rcity')
+        raddress = request.POST.get('raddress')
+        rmobile = request.POST.get('rmobile')
+        veg = request.POST.get('veg')
+        nchefs = request.POST.get('nchefs')
+        # rdate = request.get('rdate']
         rimg1 = request.FILES.get('rimg1')
         rimg2 = request.FILES.get('rimg2')
         rimg3 = request.FILES.get('rimg3')
         rimg4 = request.FILES.get('rimg4')
-        desc = request.POST['desc']
+        desc = request.POST.get('desc')
 
         user_obj = User.objects.get(username=uname)
 
@@ -55,6 +57,79 @@ def addRestaurant(request):
 
     return render(request, 'foodprovider/add_restaurant.html')
 
+# Edit restaurant
+def editRestaurant(request):
+    pass
+
+# Delete restaurant
+def deleteRestaurant(request, id):
+    if not request.user.is_authenticated:
+        return redirect("/login/")
+    
+    restaurant = Restaurant.objects.get(id=id)
+    restaurant.delete()
+    return redirect('/foodprovider/dashboard/')
+
+# Add Menu Function
+def addMenu(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
+    rest_user = User.objects.get(username=request.user)
+    restaurant_name = Restaurant.objects.filter(user=rest_user)
+
+    if request.method == 'POST':
+        rname = request.POST.get('rname')
+        mname = request.POST.get('mname')
+        mtype = request.POST.get('mtype')
+        mprice = request.POST.get('mprice')
+        mimg1 = request.FILES.get('mimg1')
+        mdesc = request.POST.get('mdesc')
+
+        for i in restaurant_name:
+            print(i, " and ", rname)
+            if i.name == rname:
+                print("Chal gaya")
+                menu = Menu.objects.create(restaurant=i, name=mname, 
+                                        type=mtype, price=mprice, img1=mimg1, description=mdesc)
+                menu.save()
+                message = messages.success(request, 'Menu added successfully!')
+                print("Ynha tak")
+                break
+
+    context = {
+        'restaurant_name' : restaurant_name
+    }
+    return render(request, 'foodprovider/add_menu.html', context)
+
+# View Individual restaurants menu's
+def viewMenu(request, id):
+    # get restaurant name
+    restaurant = Restaurant.objects.get(id=id)
+
+    # get all the menu's of restaurant
+    all_menus = Menu.objects.filter(restaurant=restaurant)
+
+    context = {
+        "all_menus" : all_menus
+    }
+
+    return render(request, 'foodprovider/view_menu.html', context)
+
+# Edit Menu
+def editMenu(request):
+    pass
+
+# Delete Menu
+def deleteMenu(request, id):
+    if not request.user.is_authenticated:
+        return redirect("/login/")
+    
+    # get a single menu item
+    menu_item = Menu.objects.get(id=id)
+    menu_item.delete()
+    return redirect('/foodprovider/viewMenu/')
+
 # Function to get all restaurant data
 def restaurant(request):
     # get all restaurant data
@@ -64,3 +139,17 @@ def restaurant(request):
         'restaurants':rst,
     }
     return render(request, 'foodprovider/restaurant.html', context) 
+
+# Individual Restaurant Information
+def restaurant_info(request, id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+    
+    rest_id = Restaurant.objects.get(id=id)
+    rest_id_menus = Menu.objects.filter(restaurant=rest_id)
+    context = {
+        'rest_id' : rest_id,
+        'rest_id_menus' : rest_id_menus
+    }
+
+    return render(request, 'foodprovider/restaurant_info.html', context) 
