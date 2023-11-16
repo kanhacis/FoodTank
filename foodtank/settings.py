4f4799a9f120472b6cd41c5cc4c5d2b20235ff59
kanhacis <kanha.p@cisinlabs.com>
2023-11-16 20:31:54 +0530

    16.1

--- foodtank/settings.py
+++ foodtank/settings.py
@@ -51,12 +51,6 @@ INSTALLED_APPS = [
     # 'order',
 ]
 
-# below codes are for static files
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -135,9 +129,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 STORAGES = {
     "default": {
@@ -150,9 +147,6 @@ STORAGES = {
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
 
 
 
